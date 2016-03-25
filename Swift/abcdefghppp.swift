bc197a226b725fce8f8a78cab87e3376cf002e61
evec <sai_cheong@hotmail.com>
2016-03-25 21:51:39 +0800

    Swift version of AB - CD = EF + GH = PPP
     AB - CD = EF + GH = PPP
    Example for Swift as use backward finding.

---
+++ Swift/abcdefghppp.swift
@@ -0,0 +1,37 @@
+//: Tested on Xcode 7.3 Playground
+
+import Cocoa
+
+
+var p = 1;
+
+// g start from 1 because no 0 and p=1
+for g in 2...9{
+    for h in 0...9{
+        if h==p||g==h{
+            continue}
+        // ef = 111-gh
+            var ef = (p*100+p*10+p)-(g*10+h)
+            var e = ef/10
+            var f = ef%10
+        
+        //ef + cd = ab
+            for c in 1...9{
+                for d in 0...9{
+                    if c==p||d==p||c==e||d==e||c==f||d==f||c==g||d==g||c==f||d==f||c==d{
+                        continue}
+                        var ab = ef+c*10+d
+                        if ab>99{
+                            continue}
+                    
+                            var a = ab/10
+                            var b = ab%10
+                                if a==p||b==p||a==e||b==e||a==f||b==f||a==g||b==g||a==f||b==f||a==c||b==c||a==d||a==d||a==b{
+                                    continue}
+                                print("AB \(a)\(b)- CD \(c)\(d) = EF \(e)\(f) +GH \(g)\(h) = \(p)\(p)\(p)")
+                    
+                }
+            }
+        
+    }
+}
